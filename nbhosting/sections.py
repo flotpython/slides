6f9388f8d1532fc8c7f07b2df25f4f0fa69b2556
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-02-22 21:19:56 +0100

    sectioning for nbhosting

---
+++ nbhosting/sections.py
@@ -0,0 +1,37 @@
+# pylint: disable=c0111
+from nbhosting.courses import (
+    Sections, Section, Notebook,
+    notebooks_by_pattern, sections_by_directory)
+
+def sections(coursedir, track):
+    """
+    coursedir is a CourseDir object that points 
+    at the root directory of the filesystem tree
+    that holds notebooks
+    track is a name that is set by the nbhosting admin,
+    by default it is "course" which would mean the full 
+    course, but you can define alternate tracks among the
+    course material
+
+    result should be a Sections object
+    """
+
+    # default for track is 'course'
+    if track == "course":
+        # this for now is just some random selection
+        # for testing manual sectioning
+        sections = [
+            "introduction",
+            "syntaxe & instructions",
+            "fonctions",
+            "modules & packages",
+            "classes & POO"
+            ]
+        sections = [
+            Section(coursedir=coursedir,
+                    name=section,
+                    notebooks=notebooks_by_pattern(
+                        coursedir,
+                        f"slides/{number:02}*.ipynb"))
+            for number, section in enumerate(sections, 1)]
+        return Sections(coursedir, sections)
