744714703efc6f3b55fe32477abefdcc4e568beb
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-12-16 10:26:57 +0100

    enable jupytext metadata

--- tps/metro/metro.py
+++ tps/metro/metro.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   notebookname: "m\xE9tro parisien"
+# ---
+
 # %% [markdown]
 # # le réseau du métro parisien
 
