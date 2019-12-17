fe1c531577d5a5c3a62112502af91098c80fe453
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-12-17 10:57:10 +0100

    enrichi le TP profs avant de copier pour les étudiants

--- tps/metro/metro.py
+++ tps/metro/metro.py
@@ -20,12 +20,8 @@
 # %% [markdown]
 # # le réseau du métro parisien
 
-# %%
-# %load_ext autoreload
-# %autoreload 2
-
 # %% [markdown]
-# ![](metro.png)
+# <img src="metro.png" width="600px" />
 
 # %%
 from pathlib import Path
@@ -50,11 +46,12 @@ DATA = Path("DATA")
 stations  = pd.read_csv(DATA/"stations.txt", index_col="station_id")
 hops =      pd.read_csv(DATA/"hops.txt")
 
-# %%
+# %% {"cell_style": "split"}
 stations.head()
 
-# %%
+# %% {"cell_style": "split"}
 hops.head()
+
 
 # %% [markdown]
 # **SOURCE :** à l'origine [la source de ces données est la RATP](https://dataratp2.opendatasoft.com/explore/dataset/offre-transport-de-la-ratp-format-gtfs/information/) et [plus précisément ceci](http://dataratp.download.opendatasoft.com/RATP_GTFS_LINES.zip), prétraité par nos soins
@@ -82,10 +79,65 @@ hops.head()
 #   1. afficher les résultats  
 
 # %% [markdown]
-# ## structures de données
+# ## les algorithmes de parcours
+#
+# Il y a deux algorithmes standard pour parcourir un graphe à partir de l'un de ses sommets :
+#
+# * DFS (depth-first-scan)
+# * BFS (breadth-first scan)
+#
+# intuitivement :
+# %%
+from simpletree import tree
+
+tree
+
+# %% [markdown] {"cell_style": "split"}
+# DFS donnerait l'énumération suivante :
+# ```
+# v v1 v11 v111 v112 
+# v12 v121 v122 
+# v2 v21 v211 v212 
+# v22 v221 v222
+# ```
+
+# %% [markdown] {"cell_style": "split"}
+# alors que BFS verrait au contraire :
+# ```
+# v 
+# v1 v2 
+# v11 v12 v21 v22
+# v111 v112 v121 v122 v211 v212 v221 v222
+# ```
+
 
 # %% [markdown]
-# On va représenter le graphe avec plusieurs classes d'objet :
+# pour simplifier au maximum les deux algorithmes de parcours que l'on veut implémenter, on peut les décrire informellement de la façon suivante :
+#
+# * en entrée on nous passe le sommet qui sert de point de départ `start`
+# * on initialise un ensemble vide de sommets `scanned`  
+#  qui contiendra tous les sommets qu'on a déjà parcourus
+# * on initialise une file d'attente `waiting_area`, dans laquelle on met `start`
+# * le parcours consiste alors à faire, tant que `waiting_area` n'est pas vide :
+#   * prendre (et enlever) un élément `next` de `waiting_area`  
+#     l'afficher (ou en faire ce que le parcours doit en faire)
+#   * ajouter `next` dans `scanned` pour matérialiser le fait qu'on y est déjà passé
+#   * pour tous les voisins de `waiting_area` qui ne sont pas encore dans `scanned`, les ajouter dans `waiting_area`
+
+# %% [markdown]
+# ce qui est assez remarquable, c'est que les deux ordres de parcours (DFS et BFS) peuvent être toutes les deux réalisés avec ce même algorithme, la seule chose qui change entre les deux étant l'ordre dans lequel `waiting_area` "rend" les choses qu'on y insère
+#
+# ainsi si `waiting_area` est une FILO (first-in-last-out), l'algorithme ci-dessus fait un parcours DFS
+#
+# et inversement si `waiting_area` est une FIFO (first-in-first-out), alors on parcourt le graphe en BFS
+#
+# c'est ce que nous visualiserons à la fin de ce TP
+
+# %% [markdown]
+# ## structures de données - intro
+
+# %% [markdown]
+# on va représenter le graphe avec plusieurs classes d'objet :
 # * `Station` : contient les détails de l'arrêt ; ici on n'a gardé, parmi tous les détails exposés par la RATP, que les coordonnées latitude et longitude, mais dans un exemple plus réaliste on pourrait avoir ici plein de détails...
 # * `Node` : modélise un noeud (sommet) dans le graphe, et ses noeuds voisins ;
 # * `Graph` : contient l'ensemble des noeuds du réseau.
@@ -94,7 +146,142 @@ hops.head()
 # ![](data-structures.png)
 
 # %% [markdown]
+# ## spécifications
+
+# %% [markdown]
+# ### specs (1) : construction du graphe
+
+# %% [markdown]
+# voici pour commencer le code qu'on **aimerait pouvoir écrire**
+
+# %%
+# on rappelle que stations et hops sont des dataframes
+# on peut itérer sur les lignes d'une dataframe avec iterrows()
+
+def build_graph(stations, hops):
+
+    graph = Graph()
+    
+    # créer un node par station
+    for index, station in stations.iterrows():
+        node = graph.add_node(station)
+        
+    # créer une arête par hop, annotée par le numéro de ligne
+    for index, hop in hops.iterrows():
+        graph.add_edge(hop['from_station_id'], hop['to_station_id'], hop['line'])
+        
+    return graph
+
+
+# %% [markdown]
+# ainsi une fois qu'on aura implémenté la classe `Graph` (et la classe `Node` sous-jacente) on pourra construire notre graphe en écrivant
+#
+# ```python
+# metro = build_graph(stations, hops)
+# ```
+
+# %% [markdown]
+# ### specs (2) : dessin de la carte
+
+# %% [markdown]
+# pour ce qui est d'afficher le réseau sur une carte, le code qu'on va vouloir écrire ressemble à ceci (on reparlera plus en détail de la librairie `folium` en temps voulu) :
+#
+# ```python
+# def build_map(metro, show_labels=True):
+#
+#     map = folium.Map(...)
+#     
+#     for node in metro.iter_nodes():
+#         # si le noeud a un label
+#         if show_labels and node.label:
+#             # l'afficher à la position du noeud
+#             # donc on a besoin d'accéder à 
+#             node.latitude, node.longitude, node.label
+#
+#     # quand on itère sur les arêtes on retourne 
+#     # un triplet avec le numéro de ligne
+#     # qu'on utilisera ici pour trouver la couleur attachée à chaque ligne
+#     for node, neighbour, line in metro.iter_edges():
+#         # pour tracer un trait entre les deux 
+#         # on a besoin d'accéder à 
+#         (node.latitude, node.longitude),
+#         (neighbour.latitude, neighbour.longitude)
+#             
+#     return map
+# ```
+
+# %% [markdown]
+# ### specs (3) : parcours
+
+# %% [markdown]
+# en ce qui concerne les algorithmes de parcours, compte tenu de ce qu'on a vu ci-dessus, le code qu'on va vouloir écrire aura de plus besoin d'itérer sur les voisins d'un noeud, et ressemblera schématiquement à ceci :
+
+# %%
+def scan(start_node, storage):
+
+    storage.store(start_node)
+    ...
+    
+    while storage:
+        current_node = storage.retrieve()
+
+        ...
+        
+        for neighbour, line in current_node.iter_neighbours():
+            storage.store(neighbour)
+
+
+# %% [markdown]
+# ### specs : résumé
+
+# %% [markdown] {"cell_style": "split"}
+# pour résumer les besoins sur les classes `Graph` et `Node` :
+#
+# ```python3
+#
+# # création
+# node = graph.add_node(station)
+#
+# graph.add_edge(
+#    from_station_id, to_station_id, line)
+#
+# # parcours des noeuds
+# for node in graph.iter_nodes():
+#    # accès à partir d'un noeud
+#    node.station, node.label
+#    node.latitude, node.longitude
+#    
+# # parcours des arêtes
+# for node, neighbour, line in graph.iter_edges():
+#    ...
+#    
+# for neighbour, line in node.iter_edges():
+#    ...
+# ```
+
+# %% [markdown] {"cell_style": "split"}
+# et on peut ajouter quelques fonctions de confort
+#
+# ```python3
+#
+# len(graph) # le nombre de noeuds
+#
+# graph.nb_edges()
+#
+# node.nb_edges()
+#
+# ```
+
+# %% [markdown]
+# ## implémentation
+
+# %% [markdown]
 # ### le type `Station`
+
+# %% [markdown]
+# pour le type Station on n'a rien à écrire, c'est l'intérêt d'avoir séparé la couche 'données' (dataframe) de la couche 'connectivité' (Graph), un objet de type `Station` est en fait une instance de `pandas.Series`
+#
+# voici comment utiliser les objets de type `Station` 
 
 # %%
 # le type Station correspond à une ligne 
@@ -110,40 +297,45 @@ station_sample = stations.loc[2152]
 station_sample
 
 # %%
+# pour accéder aux positions géographiques c'est simple
+station_sample['latitude']
+
+# %%
+# par contre pour accéder au station_id, qui sert d'index,
+# c'est différent
 try:
     station_sample['station_id']
 except Exception as exc:
     print(f"OOPS - {type(exc)} : {exc}")
 
 # %%
-station_sample['latitude']
-
-# %%
+# il faut utiliser l'attribut name (ça n'est pas très logique d'ailleurs !)
 station_sample.name
 
-# %%
+# %% {"cell_style": "split"}
 # quel est le type de cet objet
 type(station_sample)
 
-# %%
-# définissons un type
+# %% {"cell_style": "split"}
+# définissons un type pour les type hints
 Station = pd.Series
 
 
 # %% [markdown]
 # ### le type `Node`
 #
-# Pour modéliser les relations entre stations, on *enrobe* le type `Station` avec un objet `Node` qui possède une référence vers une station, et aussi vers les noeuds voisins.
+# pour modéliser les relations entre stations, on *enrobe* le type `Station` avec un objet `Node` qui possède une référence vers une station, et aussi vers les noeuds voisins
 #
-# La première idée consisterait à garder dans un attribut `self.neighbours` un `set` d'objets `Node`; mais comme on veut également attacher à chaque lien une information sur le numéro de ligne, on va plutôt utiliser un dictionnaire dans les clés sont les voisins et les valeurs les numéros de ligne.
+# la première idée consisterait à garder dans un attribut `self.neighbours` un `set` d'objets `Node` (c'est d'ailleurs ce qu'on a représenté sur le diagramme simplifié ci-dessus)
 #
-# Ce qui nous donne :
+# mais il se trouve que dans `hops.txt` on nous donne aussi le numéro de la ligne de métro qui connecte deux stations, on va donc vouloir attacher à chaque lien ce numéro de ligne, et du coup un ensemble n'est sans doute pas ce qu'il y a de mieux...
 
 # %%
 class Node:
     """
-    a node is part of a graph; it has a reference to a unique Station object
-    and also to a set of Node's that are its immediate neighbours
+    a node has a reference to a unique Station object
+    and also logically a set of neighbours, 
+    each tagged with a line among the 14 metro lines
     finally it has an optional 'label' attribute that we will use when 
     drawing the graph on a map
     """
@@ -223,9 +415,6 @@ class Graph:
     def nb_edges(self):
         return sum(node.nb_edges() for node in self.nodes)
     
-    def nb_nodes(self):
-        return len(self.nodes)
-    
     def iter_nodes(self):
         """
         an iterator on nodes
@@ -244,31 +433,25 @@ class Graph:
 # %% [markdown]
 # ## construction du graphe
 
-# %%
-def build_graph(stations, hops):
-    graph = Graph()
-    for index, station in stations.iterrows():
-        node = graph.add_node(station)
-    for index, hop in hops.iterrows():
-        graph.add_edge(hop['from_station_id'], hop['to_station_id'], hop['line'])
-    return graph
-
+# %% [markdown]
+# maintenant on devrait pouvoir construire le graphe
 
 # %%
 metro = build_graph(stations, hops)
 
-print(f"notre graphe a {metro.nb_nodes()} stations et {metro.nb_edges()} liens")
+print(f"notre graphe a {len(metro)} stations et {metro.nb_edges()} liens")
 
 # %%
 # exercice: calculer le nombre de lignes
-lines = set()
-for node, neighbour, line in metro.iter_edges():
-    lines.add(line)
+lines = {line for node, neighbour, line in metro.iter_edges()}
 
 print(lines)
 
 # %% [markdown]
 # ## dessiner le graphe sur une carte
+
+# %% [markdown]
+# on va utiliser la librairie `folium` pour afficher les cartes
 
 # %%
 import folium
@@ -287,7 +470,7 @@ from nuancier import nuancier
 nuancier
 
 # %% [markdown]
-# ### afficher les labels
+# ### afficher les labels   
 #
 # pour mettre en évidence une station (un peu fastidieux en folium)
 
@@ -311,7 +494,7 @@ map_center = [chatelet_station['latitude'], chatelet_station['longitude']]
 autre_station = stations.loc[2122]
 autre_position = [autre_station['latitude'], autre_station['longitude']]
 
-une_couleur = nuancier["1"]
+une_couleur = nuancier["7"]
 
 # %% [markdown]
 # On n'aura besoin que des classes suivantes
@@ -378,72 +561,25 @@ metro.find_node_from_station_id(chatelet_station_id).label = '0'
 build_map(metro)
 
 # %% [markdown]
-# *****
-# *****
-# *****
+# ## parcours : implémentation et illustration
 
 # %% [markdown]
-# # graph browsing
+# ### objectives
 
 # %% [markdown]
-# ## depth-first or breadth-first scanning
+# nous voulons écrire un **generateur** qui implémente les deux stratégies de parcours, à partir d'un sommet du graphe
 #
-# given a non-valued directed graph G, and a start vertex V, there are 2 famous algorithm to walk the graph from V
-#
-# * depth-first (DF) browsing, and
-# * breadth-first (BF) browsing
-#
-# intuitively :
-
-# %%
-from simpletree import tree
-
-tree
+# bien entendu, seuls les sommets accessibles seront parcourus, ce qui dans notre cas n'a pas d'importance puisque le graphe du métro est connexe
 
 # %% [markdown]
-# DF browsing from v would scan
-# ```
-# v v1 v11 v111 v112 
-# v12 v121 v122 
-# v2 v21 v211 v212 
-# v22 v221 v222
-# ```
-
-# %% [markdown]
-# BF browsing would scan
-# ```
-# v 
-# v1 v2 
-# v11 v12 v21 v22
-# v111 v112 v121 v122 v211 v212 v221 v222
-# ```
-
-# %% [markdown]
-# ## objectives
-
-# %% [markdown]
-# we want to write a **generator** that implements these 2 browsing policies from a a graph and vertex.
+# nous avons vu qu'on pouvait unifier les deux parcours, en utilisant une file d'attente FIFO ou FILO selon le parcours que l'on veut faire
 #
-# of course only the nodes reachable from the entry vertex will be browsed.
-
-# %% [markdown]
-# we want to write a **generator** that implements the 2 browsing policies from a vertex in a graph :
-# * depth first search (DF or DFS)
-# * breadth first search (BF or BFS)
-#
-# of course only the nodes reachable from the entry vertex will be browsed.
-
-# %% [markdown]
-# ## algorithms
-#
-# the algorithms used to perform these scans are, interestingly, very close to one another
-#
-# in both cases we need a STORAGE object, where we can store things and retrieve them later on
+# nous avons simplement besoin dans les deux cas d'un objet `Storage` qui implémente `store()` et `retrieve()` avec la bonne politique; aussi de manière accessoire on a besoin de pouvoir tester si une file est vide ou non, c'est à dire de pouvoir écrire `while storage: ...`
 
 # %% [markdown]
 # ### FIFO / FILO
 
-# %%
+# %% {"cell_style": "split"}
 from collections import deque
 class Fifo:
     def __init__(self):
@@ -457,7 +593,7 @@ class Fifo:
         return len(self.line)
 
 
-# %%
+# %% {"cell_style": "split"}
 from collections import deque
 class Filo:
     def __init__(self):
@@ -471,7 +607,7 @@ class Filo:
         return len(self.line)        
 
 
-# %%
+# %% {"cell_style": "split"}
 fifo = Fifo()
 for i in range(1, 4):
     fifo.store(i)
@@ -479,7 +615,7 @@ while fifo:
     print(f"retrieve → {fifo.retrieve()}")
 
 
-# %%
+# %% {"cell_style": "split"}
 filo = Filo()
 for i in range(1, 4):
     filo.store(i)
@@ -487,10 +623,14 @@ while filo:
     print(f"retrieve → {filo.retrieve()}")
 
 
+# %% [markdown]
+# ### parcours générique
+
 # %%
-# assumptions:
-# vertices reachable from a vertex are 
-# stored in a 'neighbours' attribute
+# avec nos spécifications, on peut écrire le parcours 
+# en utilisant principalement
+# for neighbour, line in node.iter_neighbours():
+# 
 def scan(start_node, storage):
     """
     scan all vertices reachable from start vertex
@@ -518,6 +658,9 @@ def scan(start_node, storage):
             storage.store(neighbour)
 
 
+# %% [markdown]
+# ### les deux parcours spécifiques
+
 # %%
 def DFS(metro, station):
     node = metro.find_node_from_station_id(station.name)
@@ -533,10 +676,13 @@ def BFS(metro, station):
 
 
 # %% [markdown]
-# ## illustration
+# ### illustration
 
 # %% [markdown]
-# ### depth-first scan
+# pour illustrer les deux parcours, on va simplement utiliser l'attribut `label` des noeuds, et y ranger l'ordre dans lequel se fait le parcours
+
+# %% [markdown]
+# #### depth-first scan
 
 # %%
 # labelling all stations according to a DFS scan
@@ -546,7 +692,7 @@ print(f"index={index}")
 build_map(metro)
 
 # %% [markdown]
-# ### breadth-first scan
+# #### breadth-first scan
 
 # %%
 # same with a BFS
