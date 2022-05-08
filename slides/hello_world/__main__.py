179178ccc611e47c96dfc5ce5482d5995a84557f
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-05-08 16:17:02 +0200

    fix hello_world

--- slides/hello_world/__main__.py
+++ slides/hello_world/__main__.py
@@ -1,3 +1,13 @@
-from .run import main
+# run this program by doing
+#
+# python /the/folder/to/here/
+# without the filename
+#
+# it will locate the file __main__.py as its entry point
+#
+# also the folder in question is in sys.path
+# and so we can import just 'run'
+
+from run import main
 
 main()
