3470d156a1028c32d62a2debee19e649b50dc9d0
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-02-07 19:20:30 +0100

    review 2-4

--- slides/2-4-references-nb.py
+++ slides/2-4-references-nb.py
@@ -3,8 +3,11 @@
 # jupyter:
 #   celltoolbar: Slideshow
 #   jupytext:
-#     cell_metadata_filter: all,-hidden,-heading_collapsed,-run_control,-trusted
-#     notebook_metadata_filter: all,-language_info,-toc,-jupytext.text_representation.jupytext_version,-jupytext.text_representation.format_version
+#     cell_metadata_filter: all,-hidden,-heading_collapsed,-run_control,-trusted,-editable
+#     notebook_metadata_filter: all, -jupytext.text_representation.jupytext_version,
+#       -jupytext.text_representation.format_version,-language_info.version, -language_info.codemirror_mode.version,
+#       -language_info.codemirror_mode,-language_info.file_extension, -language_info.mimetype,
+#       -toc, -version
 #     text_representation:
 #       extension: .py
 #       format_name: percent
@@ -12,6 +15,10 @@
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
+#   language_info:
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
 #   nbhosting:
 #     title: "r\xE9f\xE9rences partag\xE9es"
 #   rise:
@@ -28,20 +35,18 @@
 from IPython.display import HTML
 HTML(filename="_static/style.html")
 
-# %% [markdown] slideshow={"slide_type": ""}
-# # références partagées
-
 # %% [markdown]
-# pour visualiser le comportement de nos programmes  
-# et notamment cet aspect de partage de la mémoire
+# # références partagées
+#
+# pour visualiser le comportement de nos programmes, et notamment cet aspect de partage de la mémoire  
+# nous allons utiliser des illustrations produites par l'excellent <https://pythontutor.com>
 
 # %%
 # %load_ext ipythontutor
 
-# %% [markdown] slideshow={"slide_type": "slide"}
-# ## composition des types de base
-
 # %% [markdown]
+# ## composition des types de base
+#
 # * tous ces containers peuvent être imbriqués  
 #   liste de dictionnaires de …
 #
@@ -51,72 +56,71 @@ HTML(filename="_static/style.html")
 
 # %% slideshow={"slide_type": "slide"}
 # %%ipythontutor heapPrimitives=true curInstr=1 width=900 height=850
+
 # une liste avec une sous-liste qui contient un dict et un tuple
 L = ['abc', [ { (1, 2) : 1}, ([3], 4)], 5]
 
-# %% [markdown] slideshow={"slide_type": "slide"}
-# ## typage dynamique
-
 # %% [markdown]
+# ## typage dynamique
+#
 # * si on exécute `a = 3`
 # * Python va
 #   * créer un objet représentant 3
 #   * créer une variable `a` si elle n’existe pas encore  
 #     (une entrée dans une table)
 #
-#   * faire de `a` une **référence** de l’objet
+#   * faire de `a` une **référence** de l’objet (ou vers l'objet, si vous préférez)
 
 # %% slideshow={"slide_type": "slide"}
 # %%ipythontutor heapPrimitives=true width=800 curInstr=1
 a = 3
 
-# %% [markdown] slideshow={"slide_type": "slide"}
-# ## références partagées
-
 # %% [markdown]
-# du coup on peut **facilement** avoir  
-# **plusieurs variables**  
-# qui référencent le **même** objet
+# ## références partagées
+#
+# du coup on peut **facilement** avoir **plusieurs variables** qui référencent le **même** objet
 
 # %% slideshow={"slide_type": "slide"}
-# %%ipythontutor heapPrimitives=true width=800 curInstr=1
+# %%ipythontutor heapPrimitives=true width=800 curInstr=2
 
 # on aurait pu écrire aussi
 # a = b = 3
 a = 3
 b = a
 
-# %% [markdown] slideshow={"slide_type": "slide"}
+# %% [markdown]
 # ## rappel : mutable *vs* immutable
+#
+# les entiers, les chaines, les tuples sont `immutables`
+#
+# * ils ne **peuvent pas** être modifiés
+# * il n’y a **pas d’effet de bord** possible sur un objet immutable
+# * il est impossible qu’une modification sur `b` affecte `a`
 
 # %% [markdown]
-# * les entiers, les chaines, les tuples sont `immutables`
-#   * ils ne **peuvent pas** être modifiés
-#   * il n’y a **pas d’effet de bord** possible  
-#     sur un objet immutable
-#
-#   * il est impossible qu’une modification sur `b` affecte `a`
-
-# %% [markdown] slideshow={"slide_type": "slide"}
 # ### références partagées vers objet immutable
+#
+# ````{admonition} b += 1
+# :class: warning
+#
+# du coup dans notre exemple, si je faisais à présent `b += 1`, et contrairement à ce que vous pourriez penser 
+# si vous avez fait du C++, on se retrouverait avec `b` qui vaut 4 et `a` qui vaut toujours 3 !
+# ````
 
 # %% slideshow={"slide_type": "-"}
 # %%ipythontutor heapPrimitives=true width=800 curInstr=1
 
 a = 3
 b = a
-a = a + 2
-
-# %% [markdown] slideshow={"slide_type": "slide"}
-# ### idem avec objet mutable
+b += 1
 
 # %% [markdown]
-# * comme avant, deux références vers le même objet
-# * mais que se passe-t-il si l’objet est mutable ?
-#   * il peut **être changé**
-# * → impact sur **toutes** les références vers cet objet
-#   * depuis une variable
-#   * ou depuis l'intérieur d'un autre objet
+# ### idem avec objet mutable
+#
+# on prend de nouveau deux références vers le même objet  
+# mais que se passe-t-il si l’objet est mutable ? eh bien il peut **être changé**  
+# et alors cela impacte **toutes** les références vers cet objet  
+# que ce soit depuis une variable, ou depuis l'intérieur d'un autre objet
 
 # %% cell_style="center" slideshow={"slide_type": "slide"}
 # %%ipythontutor heapPrimitives=true width=800 height=300 curInstr=1
@@ -141,19 +145,17 @@ a[0] = 'spam'
 # | **`frozenset`** | non |
 
 # %% [markdown] slideshow={"slide_type": ""}
-# ````{admonition} xxx
+# ````{admonition} rappel
 #
-# les types `tuple` et `frozenset` permettent notamment  
-# de construire des **clés** pour les dictionnaires et autres ensembles
+# les types `tuple` et `frozenset` permettent notamment de construire des **clés** pour les dictionnaires et ensembles
 #
 # ````
 
-# %% [markdown] slideshow={"slide_type": "slide"}
-# ## *shallow* et *deep* copies
-
 # %% [markdown]
-# * pour ne pas modifier `b`, faire une **copie** de `a`  
-#   il y a deux types de copies en Python:
+# ## *shallow* et *deep* copies
+#
+# la solution pour ne pas modifier `b`: faire une **copie** de `a`  
+# il y a deux types de copies en Python:
 # * la *shallow copy* (superficielle)
 #   * pour les toutes les séquences, utiliser simplement `a[:]`
 #   * pour les dictionnaires utiliser `D.copy()`
@@ -161,22 +163,19 @@ a[0] = 'spam'
 # * la *deep copy* (profonde)
 #   * `copy.deepcopy()` pour tout copier de manière récursive
 
-# %% [markdown] slideshow={"slide_type": "slide"}
-# ### *shallow* et *deep* copies
-
 # %% [markdown]
+# ### différence
+#
 # * la différence entre *shallow* et *deep* copy n’existe que pour les  
 #   objets composites (les objets qui contiennent d’autres objets)
-#
-# * shallow copy
-#   * crée un nouvel objet composite et y insère  
-#     les **références** vers les objets contenus dans l’original
-#
-# * deep copy
-#   * crée un nouvel objet et insère de **manière récursive**  
-#     une copie des objets trouvés dans l’original
-#
-#   * évite les boucles infinies
+# * ***shallow copy***: crée un nouvel objet composite et y insère  
+#   les **références** vers les objets contenus dans l’original
+# * ***deep copy***: crée un nouvel objet et insère de **manière récursive**  
+#   une copie des objets trouvés dans l’original  
+#   évite les boucles infinies
+
+# %% [markdown]
+# ### exemple d'utilisation de *shallow copy*
 
 # %% cell_style="center" slideshow={"slide_type": "slide"}
 # %%ipythontutor heapPrimitives=true height=400 width=800 curInstr=1
@@ -190,71 +189,73 @@ b = a[:]
 # b n'est pas modifié
 a[0] = 'spam'
 
-# %% [markdown] slideshow={"slide_type": "slide"}
-# ## *shallow* et *deep*
+# %% [markdown]
+# ````{admonition} remarque
+#
+# dans ce cas de figure, la donnée de départ n'est pas assez profonde pour qu'il puisse y avoir une différence entre les deux modes de copie
+# ````
 
 # %% [markdown] cell_style="split"
-# ### *shallow*
+# ### illustration *shallow*
 #
-# ![](../media/copy-1-shallow.svg)
+# ```{image} ../media/copy-1-shallow.svg
+# :align: center
+# :width: 500px
+# ```
 
 # %% [markdown] cell_style="split"
-# ### *deep*
+# ### illustration *deep*
 #
-# ![](../media/copy-2-deep.svg)
+# ```{image} ../media/copy-2-deep.svg
+# :align: center
+# :width: 500px
+# ```
 
 # %% [markdown] slideshow={"slide_type": "slide"} cell_style="center"
-# ## `is` et `==`
+# ### rappel: `is` et `==`
 
-# %% [markdown]
+# %% [markdown] tags=["gridwidth-1-2"]
 # * `obj1 is obj2`
 #   * ssi obj1 et obj2 sont **le même objet**
 #   * forme inverse: `obj1 is not obj2`
 
-# %% [markdown]
+# %% [markdown] tags=["gridwidth-1-2"]
 # * `obj1 == obj2`
 #   * ssi **les valeurs des objets sont égales**
 #   * forme inverse `obj1 != obj2`
 
-# %% [markdown] slideshow={"slide_type": "slide"}
-# ### `is` et `==`
-
-# %% cell_style="split" slideshow={"slide_type": ""}
+# %% cell_style="split" slideshow={"slide_type": ""} tags=["gridwidth-1-2"]
 a = [0, 1, 2]
 b = a[:]
 a is b
 
-# %% cell_style="split"
-
-
-
+# %% cell_style="split" tags=["gridwidth-1-2"]
 a == b
 
-# %% cell_style="split"
+# %% cell_style="split" tags=["gridwidth-1-2"]
 c = d = [0, 1, 2]
 c is d
 
-# %% cell_style="split"
+# %% cell_style="split" tags=["gridwidth-1-2"]
 c == a
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # ### copie profonde nécessaire ?
+#
+# voyons maintenant un cas où la donnée de départ est -un peu - plus profonde, maintenant ça fait une différence de choisir l'une ou l'autre copie
 
 # %% cell_style="center" slideshow={"slide_type": "-"}
 # %%ipythontutor heapPrimitives=true height=450 width=800 curInstr=1
 
 # cette fois a est un peu plus profond
 a = [1, [2]]
-# on ne fait qu'une copie 'shallow'
+# si on ne fait qu'une copie 'shallow'
 b = a[:]
 # on peut toujours modifier b à travers a
 a[1][0] = 'boom'
 
 print(a)
 print(b)
-
-# %% [markdown] slideshow={"slide_type": "slide"}
-# ### avec une copie profonde
 
 # %% cell_style="center" slideshow={}
 # %%ipythontutor heapPrimitives=true height=500 width=900 curInstr=2
@@ -267,17 +268,13 @@ print(a)
 print(b)
 
 # %% [markdown] slideshow={"slide_type": "slide"}
-# ## autres types de références partagées
+# ## autres types de références partagées (avancé)
 
 # %% [markdown]
-# jusqu'ici, références partagées créées par **affectation**
-#
-# il existe (plein) d'autres cas de figure :
-#
-# * appel de fonction  
-#   l'objet passé en argument à une fonction  
+# on n'a parlé jusqu'ici que de références partagées créées par **affectation**  
+# **mais** il existe (plein) d'autres cas de figure :
+# * appel de fonction: l'objet passé en argument à une fonction  
 #   se retrouve affecté au **paramètre** dans la **fonction**
-#
 # * se méfier aussi des références **entre objets**
 
 # %% [markdown] slideshow={"slide_type": "slide"}
@@ -293,14 +290,11 @@ def foo(x):
 foo(L)
 print(L)
 
-# %% [markdown] slideshow={"slide_type": "slide"} tags=["level_intermediate"]
-# ### références entre objets
-
 # %% [markdown] tags=["level_intermediate"]
-# toutes les parties d'objet qui sont atteignables à partir d'autres objets peuvent devenir des références partagées
+# ### références entre objets
 #
-# pas besoin qu'il y ait nécessairement plusieurs variables dans le paysage
-#
+# toutes les parties d'objet qui sont atteignables à partir d'autres objets peuvent devenir des références partagées  
+# pas besoin qu'il y ait nécessairement plusieurs variables dans le paysage  
 # on peut le voir sur l'exemple pathologique suivant
 
 # %% cell_style="center" slideshow={"slide_type": "slide"} tags=["level_intermediate"]
@@ -312,10 +306,9 @@ print(f"repete avant {repete}")
 repete[0][0] = 1
 print(f"repete après {repete}")
 
-# %% [markdown] slideshow={"slide_type": "slide"} tags=["level_advanced"]
-# ### structures cycliques
-
 # %% [markdown] tags=["level_advanced"]
+# ### structures cycliques
+#
 # ainsi on peut aussi créer des structures cycliques
 
 # %% slideshow={"slide_type": ""} tags=["level_advanced"]
@@ -325,13 +318,11 @@ L = [None]
 L[0] = L
 print(L)
 
-# %% [markdown] slideshow={"slide_type": "slide"} tags=["level_advanced"]
-# ## gestion de la mémoire
-
 # %% [markdown] tags=["level_advanced"]
+# ## gestion de la mémoire (avancé)
+#
 # * Python sait réutiliser les objets  
 #   *e.g.* les petits entiers - slide suivant
-#
 # * Python sait libérer les objets non utilisés (garbage collector)
 #   * un objet sans référence est libéré
 #   * contrôle via le module `gc`
@@ -359,5 +350,5 @@ I is J   # non: partage
          # (optimisation)
 
 # %% [markdown] tags=["level_advanced"]
-# * est-ce que ça pose un problème ?
-#   * non ! l’optimisation n’est que pour des types **immutables**
+# est-ce que ça pose un problème ?  
+# non ! l’optimisation n’est que pour des types **immutables**
