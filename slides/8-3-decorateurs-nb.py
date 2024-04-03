f8a47246b0e8f9d3284eb05356f11d06276c6b6e
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-04-03 16:10:08 +0200

    split chapter 'divers' in 2: 'modules & packages' and 'divers'
    renumber accordingly
    also all the 6-7-8 notebooks are saved with the jupytext config

--- slides/8-3-decorateurs-nb.py
+++ slides/8-3-decorateurs-nb.py
@@ -3,8 +3,11 @@
 # jupyter:
 #   celltoolbar: Slideshow
 #   jupytext:
-#     cell_metadata_filter: all,-hidden,-heading_collapsed,-run_control,-trusted
-#     notebook_metadata_filter: all,-language_info,-toc,-jupytext.text_representation.jupytext_version,-jupytext.text_representation.format_version
+#     cell_metadata_filter: all,-hidden,-heading_collapsed,-run_control,-trusted,-editable
+#     notebook_metadata_filter: all, -jupytext.text_representation.jupytext_version,
+#       -jupytext.text_representation.format_version,-language_info.version, -language_info.codemirror_mode.version,
+#       -language_info.codemirror_mode,-language_info.file_extension, -language_info.mimetype,
+#       -toc, -version
 #     text_representation:
 #       extension: .py
 #       format_name: percent
@@ -12,6 +15,10 @@
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
+#   language_info:
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
 #   nbhosting:
 #     title: "d\xE9corateurs"
 #   rise:
