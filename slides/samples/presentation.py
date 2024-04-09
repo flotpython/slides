f6f5238cf494773c416d1b902189a017f74ba82e
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-04-09 10:51:28 +0200

    reviewed former 77 now 75 external tools

--- slides/samples/presentation.py
+++
@@ -1,9 +0,0 @@
-# pep8 est un programme permettant de vérifier le respect
-# des règles de présentation de la PEP8
-# on ne met qu'une seule ligne blanche
-
-# il ne faut pas mettre d'espace avant la (
-def factorial (n):
-    # et ici il manque des espaces dans les expressions
-    return 1 if n<=1 else factorial (n-1)
-
