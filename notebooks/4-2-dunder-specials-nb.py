ccf8601dc5f3b1e4cd5d6ba6294735aebf62a091
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-12-15 17:19:08 +0100

    42: add sortable and hashable examples

--- notebooks/4-2-dunder-specials-nb.py
+++ notebooks/4-2-dunder-specials-nb.py
@@ -196,15 +196,16 @@ classe['pierre'] is None
 
 
 # %% [markdown] slideshow={"slide_type": "slide"}
-# ## appel: `obj(x)`
+# ## classe *callable*: `obj(x)`
 #
 # on peut même donner du sens à `obj(x)`
 
 # %% tags=["gridwidth-1-2"]
+# make it callable
+
 class Line:
     """
-    the line of equation
-    y = ax + b
+    the line of equation y = ax + b
     """
     def __init__(self, a, b):
         self.a = a
@@ -219,16 +220,120 @@ class Line:
 
 
 # %% tags=["gridwidth-1-2"]
-# cet objet se comporte
-# comme une fonction
+# cet objet se comporte comme une fonction
 
 line = Line(2, 2)
 
 
-# c'est intéressant de pouvoir l'appeler
-# comme si c'était une fonction
+# du coup c'est intéressant de pouvoir l'appeler
+# comme si c'était réellement une fonction
 
 line(1)
+
+
+# %% [markdown]
+# ## classe *sortable*: `obj < obj2`
+#
+# pour ne pas changer d'exemple, imaginons que l'on veuille pouvoir **trier** une collection d'instances de `Line`  
+# pour cela il suffit d'expliciter l'ordre entre les instances  
+# et pour cela une technique consiste à redéfinir la *dunder* `__lt__` (pour *lower than*)
+#
+# ça se présenterait comme suit
+
+# %%
+# make it sortable
+
+class Line:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __repr__(self):
+        return f"{self.a}x + {self.b}"
+
+
+    # on définit l'ordre entre éléments, ici: self < other
+    
+    def __lt__(self, other):
+        return (self.a, self.b) < (other.a, other.b)
+
+
+lines = [ Line(3, 1), Line(1, 2), Line(3, -1), Line(2, 0)]
+
+# %%
+# du coup la classe sait comparer deux éléments entre eux
+
+# prenons par exemple les deux premiers éléments
+L1, L2, *_ = lines
+
+# et comparons-les
+L1 < L2
+
+# %%
+# du coup on peut trier ces éléments sans avoir à préciser la fonction de tri
+
+sorted(lines)
+
+
+# %% [markdown]
+# ````{admonition} trier, mais pas que
+# :class: tip
+# et pour le même prix on peut ainsi utiliser également tous les algorithmes qui reposent sur un ordre entre les éléments;  
+# par exemple `PriorityQueue`/`heapq` pour les queues de priorité, ou encore `bisect` pour la recherche binaire, ...
+# ````
+#
+# ````{admonition} 1 sur 6
+# :class: dropdown info
+#
+# pour notre exemple simple cela suffit bien, mais en toute rigueur pour pouvoir comparer deux objets il faudrait définir 6 opérateurs (`<`, `<=`, `>`, `>=`, `==` et `!=`); pour davantage de détails voyez notamment cette page 
+# <https://docs.python.org/3/library/functools.html#functools.total_ordering>
+# ````
+
+# %% [markdown]
+# ## classe *hashable*: D[obj]
+#
+# on a pu dire que les clés des dictionnaires devaient être des objets *non mutables*; c'est vrai pour les types de base du langage  
+# par contre lorsqu'il s'agit de classes *user-defined*, cette contrainte est levée si la classe implémente le **protocole dit des objets *hashables***
+#
+# toujours avec notre exemple de la classe `Line`: on pourrait avoir envie de créer des ensembles d'objets de type `Line`; ou bien encore d'utiliser un objet `Line` comme un clé de dictionnaire
+#
+# cela est possible par exemple comme ceci: la classe doit implémenter deux *dunder* méthodes `__eq__` et `__hash__`
+
+# %%
+# make it hashable
+
+class Line:
+    
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+    # pour définir à quelle condition deux objets
+    # sont considérés égaux - au sens de == 
+    
+    def __eq__(self, other):
+        return (self.a == other.a) and (self.f == other.b)
+
+
+    # comment doit-on calculer le hash d'un objet Line ?
+
+    def __hash__(self):
+        # on n'a qu'à le hasher comme le tuple (a, b)
+        return hash( (self.a, self.b) )
+
+# %%
+# on peut alors utiliser les objets dans un dict ou dans un set !
+
+D, S = {}, set()
+line = Line(0, 0)
+
+D[line] = "Yes !"
+S = {line, line}
+
+print(f"{D=}, {len(S)=}")
+    
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # ## résumé
